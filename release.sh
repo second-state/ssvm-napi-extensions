af831a3def5faa20e6420b10ae5b778f7f3e007b
hydai <hydai@secondstate.io>
2020-07-29 13:08:01 +0000

    [Addon] Update release script for ssvm-napi-storage

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
+./preinstall.sh
 CXX=g++-9 node-pre-gyp install --fallback-to-build --update-binary
-rm -rf ssvm
-mkdir ssvm
-cp build/Release/ssvm.node ssvm
-strip ssvm/ssvm.node
-tar zcvf ssvm-linux-x64.tar.gz ssvm
+rm -rf tmp/ssvm-storage
+mkdir -p tmp/ssvm-storage
+cp build/Release/ssvm-storage.node tmp/ssvm-storage
+strip tmp/ssvm-storage/ssvm-storage.node
+cd tmp/
+tar zcvf ../ssvm-storage-linux-x64.tar.gz ssvm-storage
+cd ../
