dbbb1cf213c88959e0ab032888ef1a7d7e3c614c
hydai <hydai@secondstate.io>
2020-12-04 05:26:10 +0000

    [Tensorflow] add helper script to check version, if tf is not installed or too old, return error

--- preinstall.sh
+++ preinstall.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# Build SSVM-Storage dependencies
 cd ./rust_native_storage_library/
 make target/debug/librust_native_storage_library.so
 cd ../
+
+# Check TensorFlow version
+c++ ./utils/checker/tensorflow_version_checker.cc -ltensorflow -o ./tf_ver
+./tf_ver
+if [ "$?" -eq "0" ]
+then
+  rm ./tf_ver
+  exit 0
+else
+  echo "TensorFlow C library is not yet installed or is too old to support"
+  exit 1
+fi
